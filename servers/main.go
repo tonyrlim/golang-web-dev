9acc3fc1e5f7fd82c53096ec25216a4259678c36
Tony Lim <tonyrlim97@gmail.com>
2024-03-12 15:26:49 -0500

    Mini example Rotate13

--- servers/main.go
+++ servers/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
+	"strings"
 )
 
 func main() {
@@ -26,18 +26,41 @@ func main() {
 }
 
 func handle(conn net.Conn) {
-	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
-	if err != nil {
-		log.Println("CONN TIMEOUT")
-	}
+	// Reading and writing from a connection
+	// err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	// if err != nil {
+	// 	log.Println("CONN TIMEOUT")
+	// }
 
+	// scanner := bufio.NewScanner(conn)
+	// for scanner.Scan() {
+	// 	line := scanner.Text()
+	// 	fmt.Println(line)
+	// 	fmt.Fprintf(conn, "I heard you say: %s\n", line)
+	// }
+	// defer conn.Close()
+	// fmt.Println("Code got here!")
+
+	// Rot13
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
-		fmt.Fprintf(conn, "I heard you say: %s\n", line)
+		line := strings.ToLower(scanner.Text())
+		byteSlice := []byte(line)
+		rotatedLine := rotate13(byteSlice)
+
+		fmt.Fprintf(conn, "%s - %s\n\n", line, rotatedLine)
 	}
 	defer conn.Close()
+}
 
-	fmt.Println("Code got here!")
+func rotate13(byteSlice []byte) []byte {
+	var r13 = make([]byte, len(byteSlice))
+	for index, value := range byteSlice {
+		if value <= 109 {
+			r13[index] = value + 13
+		} else {
+			r13[index] = value - 13
+		}
+	}
+	return r13
 }
