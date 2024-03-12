c98a6447ac8e2a890666e78ebfec0357c9a40ec6
Tony Lim <tonyrlim97@gmail.com>
2024-03-12 15:02:21 -0500

    Read and write from connection

--- servers/main.go
+++ servers/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
+	"time"
 )
 
 func main() {
@@ -13,15 +14,30 @@ func main() {
 		log.Panic(err)
 	}
 	defer listener.Close()
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Println(err)
 		}
-		io.WriteString(conn, "\nHello from TCP server\n")
-		fmt.Fprintln(conn, "How is your day?")
-		fmt.Fprintf(conn, "Well, I hope!")
 
-		conn.Close()
+		go handle(conn)
 	}
-}	
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Println("CONN TIMEOUT")
+	}
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+		fmt.Fprintf(conn, "I heard you say: %s\n", line)
+	}
+	defer conn.Close()
+
+	fmt.Println("Code got here!")
+}
